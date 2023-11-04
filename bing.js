a35fd3ff1e5bb517074d48cd7edbca4b23466d88
guapi-exe <3051016381@qq.com>
2023-11-04 14:01:19 +0800

    更新

--- bing.js
+++ bing.js
@@ -3,11 +3,7 @@ let CreateChat = require("./lib/CreateChat")
 let CreateWs = require("./lib/CreateWs")
 let Dialogue = require("./lib/Dialogue")
 let solveCaptcha = require("./lib/solveCaptcha")
-let {createimage , getimageurl} = require("./lib/Bingimage")
-let {Create , GetChatHistory , genRanHex} = require("./lib/CreateChat")
-let {createWebSocketConnection , cleanupWebSocketConnection} = require("./lib/CreateWs")
-let {chat} = require("./lib/Dialogue")
-let {solveCaptchaOneShot} = require("./lib/solveCaptcha")
+
 
 
 
@@ -17,14 +13,5 @@ module.exports = {
     CreateChat,
     CreateWs,
     Dialogue,
-    solveCaptcha,
-    createimage,
-    getimageurl,
-    Create,
-    GetChatHistory,
-    genRanHex,
-    createWebSocketConnection,
-    cleanupWebSocketConnection,
-    chat,
-    solveCaptchaOneShot
+    solveCaptcha
 }
