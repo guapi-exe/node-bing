3c65117630bc6ba11ef9200eed024e607771dd4a
guapi-exe <3051016381@qq.com>
2023-11-04 11:48:07 +0800

    更新delay库版本下调

---
+++ bing.js
@@ -0,0 +1,16 @@
+let Bingimages = require("./lib/Bingimage")
+let CreateChat = require("./lib/CreateChat")
+let CreateWs = require("./lib/CreateWs")
+let Dialogue = require("./lib/Dialogue")
+let solveCaptcha = require("./lib/solveCaptcha")
+
+
+
+
+module.exports = {
+    Bingimages,
+    CreateChat,
+    CreateWs,
+    Dialogue,
+    solveCaptcha
+}
